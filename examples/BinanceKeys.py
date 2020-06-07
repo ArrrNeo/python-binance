e27fdec55650b024c9bd10acabcf8617380e77eb
Naveen Rawat <rawat.nav@gmail.com>
2020-06-07 13:01:10 -0700

    remove hide binance keys from repo

--- examples/BinanceKeys.py
+++ examples/BinanceKeys.py
@@ -2,6 +2,9 @@
 The BinanceKeys.py Program is for keeping track of Binance Keys for use with RoibalBot.py and
 save_historical_data_Roibal.py
 """
+api_key_file = open('api_key', 'r')
+api_secret_file = open('api_secret', 'r')
 
-BinanceKey1 = {'api_key': '',
-    'api_secret':''}
+BinanceKey1 = {}
+BinanceKey1['api_key'] = api_key_file.read()
+BinanceKey1['api_secret'] = api_secret_file.read()
